9de9cc729a41191c34a8c765ba80679eb8bb9b50
Claudio Davila <davila11claudio@hotmail.com>
2025-10-22 09:30:24 -0400

    arreglo de serializador y borrador de primera funcion post

---
+++ backend/data_uploader/products/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Products
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+
+        fields = [
+            'serial_number',
+            'description',
+            'category',
+            'brand',
+            'type',
+            'quantity',
+            'location'
+        ]
